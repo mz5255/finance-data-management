409d8df5a50d2820b0abb0e4d195569e49f94045
马震 <13357428+mz5255@user.noreply.gitee.com>
2025-09-18 16:16:29 +0800

    依赖层级管理

--- finance-application/src/main/java/cn/com/mz/app/finance/application/webapi/AuthController.java
+++ finance-application/src/main/java/cn/com/mz/app/finance/application/webapi/AuthController.java
@@ -1,65 +1,65 @@
-package cn.com.mz.app.finance.application.webapi;
-
-import cn.com.mz.app.finance.common.dto.base.BaseResult;
-import cn.com.mz.app.finance.common.exceptions.BusinessException;
-import cn.com.mz.app.finance.module.dto.req.RegisterParam;
-import cn.com.mz.app.finance.module.service.auth.AuthService;
-import cn.com.mz.app.finance.module.service.auth.UserOperatorResponse;
-import cn.com.mz.app.finance.module.vo.UserRegisterRequest;
-import cn.com.mz.app.finance.starter.constant.CacheConstant;
-import jakarta.validation.Valid;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-
-/**
- * @author mz
- * @project finance-data-management
- * @package cn.com.mz.app.finance.application.webapi
- * @date 2025/9/14 22:15
- * @description: 功能描述
- */
-@RestController
-@RequestMapping("/api/finance-data/auth")
-public class AuthController {
-
-    @Resource
-    private StringRedisTemplate redisTemplate;
-    @Resource
-    private AuthService authService;
-
-
-    @PostMapping("/register")
-    public BaseResult<Boolean> register(@Valid @RequestBody RegisterParam registerParam) {
-
-        //验证码校验
-        String cachedCode = redisTemplate.opsForValue().get(CacheConstant.CAPTCHA_KEY_PREFIX + registerParam.getTelephone());
-        if (!StringUtils.equalsIgnoreCase(cachedCode, registerParam.getCaptcha())) {
-            throw new BusinessException("验证码错误");
-        }
-
-        //注册
-        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
-        userRegisterRequest.setTelephone(registerParam.getTelephone());
-        userRegisterRequest.setInviteCode(registerParam.getInviteCode());
-
-        UserOperatorResponse registerResult = authService.register(userRegisterRequest);
-        if(registerResult.isSuccess()){
-            return BaseResult.success(true);
-        }
-        return BaseResult.error(registerResult.getCode(), registerResult.getMessage());
-    }
-
-    /**
-     * 登录方法
-     *
-     * @return 结果
-     */
+//package cn.com.mz.app.finance.application.webapi;
+//
+//import cn.com.mz.app.finance.common.dto.base.BaseResult;
+//import cn.com.mz.app.finance.common.exceptions.BusinessException;
+//import cn.com.mz.app.finance.module.dto.req.RegisterParam;
+//import cn.com.mz.app.finance.module.service.auth.AuthService;
+//import cn.com.mz.app.finance.module.service.auth.UserOperatorResponse;
+//import cn.com.mz.app.finance.module.vo.UserRegisterRequest;
+//import cn.com.mz.app.finance.starter.constant.CacheConstant;
+//import jakarta.validation.Valid;
+//import org.apache.commons.lang3.StringUtils;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * @author mz
+// * @project finance-data-management
+// * @package cn.com.mz.app.finance.application.webapi
+// * @date 2025/9/14 22:15
+// * @description: 功能描述
+// */
+//@RestController
+//@RequestMapping("/api/finance-data/auth")
+//public class AuthController {
+//
+//    @Resource
+//    private StringRedisTemplate redisTemplate;
+//    @Resource
+//    private AuthService authService;
+//
+//
+//    @PostMapping("/register")
+//    public BaseResult<Boolean> register(@Valid @RequestBody RegisterParam registerParam) {
+//
+//        //验证码校验
+//        String cachedCode = redisTemplate.opsForValue().get(CacheConstant.CAPTCHA_KEY_PREFIX + registerParam.getTelephone());
+//        if (!StringUtils.equalsIgnoreCase(cachedCode, registerParam.getCaptcha())) {
+//            throw new BusinessException("验证码错误");
+//        }
+//
+//        //注册
+//        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+//        userRegisterRequest.setTelephone(registerParam.getTelephone());
+//        userRegisterRequest.setInviteCode(registerParam.getInviteCode());
+//
+//        UserOperatorResponse registerResult = authService.register(userRegisterRequest);
+//        if(registerResult.isSuccess()){
+//            return BaseResult.success(true);
+//        }
+//        return BaseResult.error(registerResult.getCode(), registerResult.getMessage());
+//    }
+//
+//    /**
+//     * 登录方法
+//     *
+//     * @return 结果
+//     */
 //    @PostMapping("/login")
 //    public Result<LoginVO> login(@Valid @RequestBody LoginParam loginParam) {
 //        //fixme 为了方便，暂时直接跳过
@@ -109,9 +109,9 @@ public class AuthController {
 //        StpUtil.logout();
 //        return Result.success(true);
 //    }
-
-    @RequestMapping("test")
-    public String test() {
-        return "test";
-    }
-}
+//
+//    @RequestMapping("test")
+//    public String test() {
+//        return "test";
+//    }
+//}
--- finance-module/module-auth/src/main/java/cn/com/mz/app/finance/module/service/auth/impl/AuthServiceImpl.java
+++ finance-module/module-auth/src/main/java/cn/com/mz/app/finance/module/service/auth/impl/AuthServiceImpl.java
@@ -22,49 +22,49 @@ public class AuthServiceImpl {
      */
     private RBloomFilter<String> nickNameBloomFilter;
 
-    @DistributeLock(keyExpression = "#telephone", scene = "USER_REGISTER")
-    @Transactional(rollbackFor = Exception.class)
-    public UserOperatorResponse register(String telephone, String inviteCode) {
-        String defaultNickName;
-        String randomString;
-        do {
-            randomString = RandomUtil.randomString(6).toUpperCase();
-            //前缀 + 6位随机数 + 手机号后四位
-            defaultNickName = randomString + telephone.substring(7, 11);
-        } while (nickNameExist(defaultNickName) || inviteCodeExist(randomString));
-
-        String inviterId = null;
-        if (StringUtils.isNotBlank(inviteCode)) {
-            User inviter = userMapper.findByInviteCode(inviteCode);
-            if (inviter != null) {
-                inviterId = inviter.getId().toString();
-            }
-        }
-
-        User user = register(telephone, defaultNickName, telephone, randomString, inviterId);
-        Assert.notNull(user, UserErrorCode.USER_OPERATE_FAILED.getCode());
-
-        addNickName(defaultNickName);
-        addInviteCode(randomString);
-        updateInviteRank(inviterId);
-        updateUserCache(user.getId().toString(), user);
-
-        //加入流水
-        long streamResult = userOperateStreamService.insertStream(user, UserOperateTypeEnum.REGISTER);
-        Assert.notNull(streamResult, () -> new BizException(RepoErrorCode.UPDATE_FAILED));
-
-        UserOperatorResponse userOperatorResponse = new UserOperatorResponse();
-        userOperatorResponse.setSuccess(true);
-
-        return userOperatorResponse;
-    }
-
-    public boolean nickNameExist(String nickName) {
-        //如果布隆过滤器中存在，再进行数据库二次判断
-        if (this.nickNameBloomFilter != null && this.nickNameBloomFilter.contains(nickName)) {
-            return userMapper.findByNickname(nickName) != null;
-        }
-
-        return false;
-    }
+//    @DistributeLock(keyExpression = "#telephone", scene = "USER_REGISTER")
+//    @Transactional(rollbackFor = Exception.class)
+//    public UserOperatorResponse register(String telephone, String inviteCode) {
+//        String defaultNickName;
+//        String randomString;
+//        do {
+//            randomString = RandomUtil.randomString(6).toUpperCase();
+//            //前缀 + 6位随机数 + 手机号后四位
+//            defaultNickName = randomString + telephone.substring(7, 11);
+//        } while (nickNameExist(defaultNickName) || inviteCodeExist(randomString));
+//
+//        String inviterId = null;
+//        if (StringUtils.isNotBlank(inviteCode)) {
+//            User inviter = userMapper.findByInviteCode(inviteCode);
+//            if (inviter != null) {
+//                inviterId = inviter.getId().toString();
+//            }
+//        }
+//
+//        User user = register(telephone, defaultNickName, telephone, randomString, inviterId);
+//        Assert.notNull(user, UserErrorCode.USER_OPERATE_FAILED.getCode());
+//
+//        addNickName(defaultNickName);
+//        addInviteCode(randomString);
+//        updateInviteRank(inviterId);
+//        updateUserCache(user.getId().toString(), user);
+//
+//        //加入流水
+//        long streamResult = userOperateStreamService.insertStream(user, UserOperateTypeEnum.REGISTER);
+//        Assert.notNull(streamResult, () -> new BizException(RepoErrorCode.UPDATE_FAILED));
+//
+//        UserOperatorResponse userOperatorResponse = new UserOperatorResponse();
+//        userOperatorResponse.setSuccess(true);
+//
+//        return userOperatorResponse;
+//    }
+//
+//    public boolean nickNameExist(String nickName) {
+//        //如果布隆过滤器中存在，再进行数据库二次判断
+//        if (this.nickNameBloomFilter != null && this.nickNameBloomFilter.contains(nickName)) {
+//            return userMapper.findByNickname(nickName) != null;
+//        }
+//
+//        return false;
+//    }
 }
